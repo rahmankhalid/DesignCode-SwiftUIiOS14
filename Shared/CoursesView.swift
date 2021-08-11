97cca4baf2e927f73e48ce2585d34e90efa06e01
Khalid Rahman <khalid.rahman.8@gmail.com>
2021-08-11 20:31:10 +0100

    init commit

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  DesignCourse
+//
+//  Created by khalid rahman on 28/10/2020.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
