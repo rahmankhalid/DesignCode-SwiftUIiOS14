9a0615c7206945419d748254ea2bcd4a67f6baae
Khalid Rahman <khalid.rahman.8@gmail.com>
2021-08-11 20:31:54 +0100

    changes

---
+++ Shared/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by khalid rahman on 05/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text("Swift UI for iOS 14").fontWeight(.bold).foregroundColor(Color.white)
+            Text("20 Sections").font(.footnote).foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+             
+                
+        }
+    }
+}
---
+++ Shared/DesignCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCourseApp.swift
+//  Shared
+//
+//  Created by khalid rahman on 05/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DesignCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
